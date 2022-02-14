4aedac93e252dc442a27141b42c85c62fe5deef1
Esther-Anyona <jkemuntoe@gmail.com>
2022-02-14 13:33:29 +0300

    Add login user form and create views

--- app/models.py
+++ app/models.py
@@ -20,16 +20,16 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy=True)
 
     @property
-        def password(self):
-            raise AttributeError('You cannot read the password attribute')
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
 
-        @password.setter
-        def password(self, password):
-            self.password_hash = generate_password_hash(password)
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
 
 
-        def verify_password(self,password):
-            return check_password_hash(self.password_hash,password)
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
 
 
     def __repr__(self):
