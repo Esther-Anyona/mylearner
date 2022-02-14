9bc1d66e2ee0572539cc84e65e3acc25664f3a85
Esther-Anyona <jkemuntoe@gmail.com>
2022-02-14 12:45:56 +0300

    Create login functions and define route for authentication

--- app/__init__.py
+++ app/__init__.py
@@ -33,7 +33,9 @@ def create_app(config_name):
     login_manager.init_app(app)
     mail.init_app(app)
 
-
+    # Blueprints
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
 
 
 
