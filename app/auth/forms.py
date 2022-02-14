0fbd141eaf2467265d027d7b9936bc00a92805fe
Esther-Anyona <jkemuntoe@gmail.com>
2022-02-14 13:05:06 +0300

    Add form to register new users

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,4 +1,23 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField
-from wtforms.validators import Required,Email,EqualTo
+from wtforms.validators import InputRequired,Email,EqualTo
 from ..models import User
+from wtforms import ValidationError
+
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[InputRequired(),Email()])
+    username = StringField('Enter your username',validators = [InputRequired()])
+    password = PasswordField('Password',validators = [InputRequired(), EqualTo('password_confirm',message = 'Passwords must match')])
+    password_confirm = PasswordField('Confirm Passwords',validators = [InputRequired()])
+    submit = SubmitField('Sign Up')
+
+    # Custom validation
+    def validate_email(self,data_field):
+        if User.query.filter_by(email =data_field.data).first():
+            raise ValidationError('An account with that email already exists')
+
+    def validate_username(self,data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('The username already exists')
