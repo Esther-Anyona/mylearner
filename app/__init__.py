e3c73ff5946ef5f39f8c352c3bde601a74760118
Esther-Anyona <jkemuntoe@gmail.com>
2022-02-14 19:59:40 +0300

    Add form to upload profile photos

--- app/__init__.py
+++ app/__init__.py
@@ -26,6 +26,8 @@ def create_app(config_name):
 # Creating the app configurations
     app.config.from_object(config_options[config_name])
 
+    # Configuring the UploadSet
+    configure_uploads(app,photos)
 
     # Initializing flask extensions
     bootstrap.init_app(app)
